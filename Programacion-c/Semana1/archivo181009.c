594dae7cc32803deaca826f8aba5032c4595c194
César Gustavo Sánchez de la Rosa <cesartavo@ciencias.unam.mx>
2018-10-09 14:13:19 -0700

    Primer Programa

---
+++ Programacion-c/Semana1/archivo181009.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+int Numero;
+int Num = 1;
+
+printf("JuanaMinerva\n");
+printf("JuanaMinerva\t");
+printf("\nMi numero es %d",Numero);
+Numero = 3;
+printf("\nMin numero es %d",Numero);
+
+}
+
+ 
